b9f4dac009191eeeb64350103af11a38aa3ef900
Vinanti Thakur <vinanti.thakur@gmail.com>
2019-07-02 19:15:28 -0400

    7/1/2019

--- Csharp/TutorialsPoint_ConsoleApp/TutorialsPoint_ConsoleApp/Operators.cs
+++ Csharp/TutorialsPoint_ConsoleApp/TutorialsPoint_ConsoleApp/Operators.cs
@@ -10,17 +10,55 @@ namespace TutorialsPoint_ConsoleApp
     {
         static void Main(string[] args)
         {
-            //Arithmatic Operators
+            Console.WriteLine("Arithmatic Operators");
             int a = 50, b = 10, c = 5;
             Console.WriteLine(a + b);
             Console.WriteLine(a - b);
             Console.WriteLine(a * b);
             Console.WriteLine(a / b);
             Console.WriteLine(a % b);
+            Console.WriteLine("------------------------1");
 
             int d = a + b * c;
             Console.WriteLine(d);
+            Console.WriteLine("------------------------2");
 
+            int a1 = 10, b1 = 20, c1 = 5;
+            int d1 = a1 + b1 + c1;
+            Console.WriteLine(d1);
+            Console.WriteLine("------------------------3");
+
+            int marks = 72, max = 80;
+            int perc = marks / max * 100;
+            Console.WriteLine(perc);
+            Console.WriteLine("------------------------4");
+
+            perc = marks * 100 / max;
+            Console.WriteLine(perc);
+            Console.WriteLine("------------------------5");
+
+            int a2 = 10;
+            a2 = a2 + 5;
+            Console.WriteLine(a2);
+            a2 += 5;
+            Console.WriteLine(a2);
+            a2++;
+            Console.WriteLine(a2);
+            Console.WriteLine("------------------------6");
+
+            int a3 = 10;
+            int b3 = ++a3;
+            Console.WriteLine(a3);
+            Console.WriteLine(b3);
+            Console.WriteLine("------------------------7");
+
+            int a4 = 10;
+            int b4 = a4++;
+            Console.WriteLine(a4);
+            Console.WriteLine(b4);
+            Console.WriteLine("------------------------7\n\n\n");
+
+            Console.WriteLine("Comparision Operators");
         }
     }
 }
