e9d454077e4047c9ff99989cb36c505d2b0766d9
Vinanti Thakur <vinanti.thakur@gmail.com>
2022-01-27 07:00:22 -0500

    01/27/2022-A

---
+++ DSA/DSAConcepts/Arrays01_CreatingAnArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DSAConcepts
+{
+    class Arrays01_CreatingAnArray
+    {
+        static void Main()
+        {
+            // The actual code for creating an Array to hold DVD's.
+            DVD[] dvdCollection = new DVD[15];
+        }
+    }
+
+
+    // A simple definition for a DVD.
+    public class DVD
+    {
+        public string name;
+        public int releaseYear;
+        public string director;
+
+        public DVD(string name, int releaseYear, string director)
+        {
+            this.name = name;
+            this.releaseYear = releaseYear;
+            this.director = director;
+        }
+
+        public string ConvertToString()
+        {
+            return this.name + ", directed by " + this.director + ", released in " + this.releaseYear;
+        }
+    }
+}
